7ded6c5752eda8397c9e7ee1a0c48b57ed746335
Hamed Hosseini <hamed8080@gmail.com>
2023-12-13 12:32:13 +0330

    - Add neshan old address for compatibility

--- Sources/ChatCore/Routes.swift
+++ Sources/ChatCore/Routes.swift
@@ -48,4 +48,6 @@ public enum Routes: String {
     case mapSearch = "/search"
     case mapRouting = "/routing"
     case mapStaticImage = "/static"
+
+    case baseMapLink = "https://maps.neshan.org/@"
 }
