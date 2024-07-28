1dae5a40429404dd276aed733a453b13ea028e9c
Hamed Hosseini <hamed8080@gmail.com>
2024-07-28 14:31:09 +0330

    Use a comment version along side with repo version to switch faster

--- Package.swift
+++ Package.swift
@@ -17,21 +17,24 @@ let package = Package(
             targets: ["ChatCore"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/async", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/async", from: "2.1.0"),
+        .package(path: "../Async"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatCore",
             dependencies: [
-                .productItem(name: "Async", package: "async"),
+//                .productItem(name: "Async", package: "async"),
+                .productItem(name: "Async", package: "Async"),
             ]
         ),
         .testTarget(
             name: "ChatCoreTests",
             dependencies: [
                 "ChatCore",
-                .productItem(name: "Async", package: "async"),
+//                .productItem(name: "Async", package: "async"),
+                .productItem(name: "Async", package: "Async"),
             ],
             resources: []
         ),
